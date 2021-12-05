d7418006a22932f0c5f39871e4c7ba5a69372fb8
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-05 03:35:37 +0000

    search functioning

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const loadBooks = function(){
         .then( books => {
             
                 renderBooks(books)
+                return books
             })
             
 }
@@ -34,29 +35,92 @@ const loadBooks = function(){
             };
         
 
+            const searchBooks1 = function(event){
+               
+               let  searchQuery = document.getElementById("search").value
+               if(searchQuery != null && searchQuery !=undefined && searchQuery !=""){
+
+                   console.log(searchQuery)
+                   filterBooks(searchQuery)
+                } else
+                alert("Please Enter the valid search")
+            }
+
 // search using enter key
 const searchBooks2 = function(event){
     let search = document.getElementById('search')
 search.addEventListener ("keyup", (event)=>{
     let searchText = event.keyCode.value
-    if(event.keyCode === 13) {
         filterBooks(searchQuery) 
-    }
+    
 })
 }
 
 // search using search button
+
 const filterBooks = function(searchQuery){
     fetch("https://striveschool-api.herokuapp.com/books")
     .then(response => response.json())
     .then( books => {
-        console.log(searchQuery)
-        console.log(books)
-        
-        const filteredBooks = books.filter((book)=>book.title.toLowerCase().includes(searchQuery.toLowerCase()))
-    console.log(filteredBooks)   
+        let row = document.querySelector('main.row')
+        row.innerHTML=""
+        const filteredBooks = (books.filter((book)=>book.title.toLowerCase().includes(searchQuery.toLowerCase())))
+        filteredBooks.forEach(book =>{
+           
+            const col = document.createElement("div")
+            col.classList.add("col-12","col-sm-6", "col-md-4","col-lg-3", "d-flex", "mt-3")
+            col.innerHTML = `<div class="card" style="width: 18rem;">
+            <div class="imgContainer">
+                <img src="${book.img}" class="card-img-top img-fluid  w-100" alt="...">
+            </div>
+        <div class="card-body">
+            <h6 class="card-title">${book.title}</h6>
+                <p class="card-text ">Category - ${book.category}</p>
+                <span class="bg-secondary text-white p-1">£ ${book.price}</span>  
+                <span class="bg-danger text-white p-1">  <small> ID${book.asin} </small></span>  
+                <div class="mt-2">
+                    <button href="#" class="btn btn-success" onclick= "addToCart(event)" id="${book.asin}">Add to cart</button>
+                        
+                </div>
+                </div>
+            </div>` 
+            row.appendChild(col)       
+        })
     })
 }
+
+
+// const filteredBooks = function(searchQuery){
+//     fetch("https://striveschool-api.herokuapp.com/books")
+//     .then(response => response.json())
+//     .then( books => {
+        
+//     let row = document.querySelector('main.row')
+//     row.innerHTML =""
+//         books.forEach(book => {
+
+           
+//             const col = document.createElement("div")
+//             col.classList.add("col-12","col-sm-6", "col-md-4","col-lg-3", "d-flex", "mt-3")
+//             col.innerHTML = `<div class="card" style="width: 18rem;">
+//             <div class="imgContainer">
+//                 <img src="${book.img}" class="card-img-top img-fluid  w-100" alt="...">
+//             </div>
+//         <div class="card-body">
+//             <h6 class="card-title">${book.title}</h6>
+//                 <p class="card-text ">Category - ${book.category}</p>
+//                 <span class="bg-secondary text-white p-1">£ ${book.price}</span>  
+//                 <span class="bg-danger text-white p-1">  <small> ID${book.asin} </small></span>  
+//                 <div class="mt-2">
+//                     <button href="#" class="btn btn-success" onclick= "addToCart(event)" id="${book.asin}">Add to cart</button>
+                        
+//                 </div>
+//                 </div>
+//             </div>`          
+//         }
+//     })
+//     })
+//         }
 
 const addToCart = function(event){
     
@@ -67,7 +131,7 @@ const addToCart = function(event){
         event.target.innerText = "Remove from cart"
     } else
     event.target.innerText = "Add to cart"
-    
+
     let col = document.querySelector("aside .row .col")
     fetch("https://striveschool-api.herokuapp.com/books")
     .then(response => response.json())
@@ -85,10 +149,10 @@ const addToCart = function(event){
             // for(let i=0; i < arrChosenBooks.length; i++){
             //   if(arrChosenBooks[i] !== id){
                 
-                col.innerHTML +=`<div class="card mb-2">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img src="${book.img}" height="70px" width="70px" alt="image of a book ${book.title} ">
+                col.innerHTML +=`<div class="card mb-2 ">
+                <div class="row no-gutters p-0 m-0">
+                  <div class="col-md-4  p-0 m-0">
+                    <img src="${book.img}"  alt="image of a book ${book.title} ">
                   </div>
                   <div class="col-md-8">
                     <div class="card-body">
@@ -108,23 +172,11 @@ const addToCart = function(event){
     })  
 })
 }
-// append to the cart
-// const displayCart = function(eventData){
-//     let cartItems = cartItems()
-//     let cart = document.querySelector("nav button:last-of-type")
-//     let row = document.querySelector("row:first-of-type")
-//     let h1 = document.querySelector("h1:first-of-type")
-//     h1.innerHTML = "My Favourite Books"
-//     cartItems.foreach(item =>{
-//         row.innerHTML += `<div class= "col-12 col-sm-6 col-md-4 col-lg-3"></dic>`
-//     })
-// }
+
 
 const skipBtn = function(event){
-    event.target.closest(".row").remove()
+    event.target.closest(".card").remove()
 }
-
-
 
 
 // hide aside with the cart key
@@ -132,25 +184,7 @@ const displayCart = function(event){
     let aside = document.querySelector("aside")
     aside.classList.toggle("hideElement")
 }
-// const filterBooks =function(){
-//     <div class="col">
-//                         <div class="card mb-3" style="max-width: 540px;">
-//                             <div class="row g-0">
-//                               <div class="col-md-4">
-//                                 <img src="${book.img}" class="img-fluid rounded-start w-100" width="" alt="...">
-//                               </div>
-//                               <div class="col-md-8">
-//                                 <div class="card-body">
-//                                   <h5 class="card-title">${book.title}</h5>
-//                                   <span class="bg-secondary text-white p-1 m-2 ">£ ${book.price}</span>  
-//                                 <span class="bg-danger text-white  p-1 m-2">  <small > ID${book.asin} </small> </span>  
-                                 
-//                                 </div>
-//                               </div>
-//                             </div>
-//                           </div>
-//                     </div>
-// }
+
 
 window.onload = function(){ 
     loadBooks()
